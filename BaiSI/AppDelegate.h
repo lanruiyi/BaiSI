f3dda81d012fd1177ab6e4a6e74423def485813a
兰瑞益 <lanruiyi@bjb-cs048.local>
2017-01-27 13:36:02 +0800

    Initial Commit

---
+++ BaiSI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSI
+//
+//  Created by 兰瑞益 on 17/1/27.
+//  Copyright © 2017年 www.lanruiyi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaiSI
+//
+//  Created by 兰瑞益 on 17/1/27.
+//  Copyright © 2017年 www.lanruiyi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
